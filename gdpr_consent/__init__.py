a98f3a1028672133e0858233b0cd75a7e86b87e2
mmaynard-code <80096908+mmaynard-code@users.noreply.github.com>
2021-03-24 17:58:18 +0100

    update gdpr text
    update gdpr text with new swedish era text

--- gdpr_consent/__init__.py
+++ gdpr_consent/__init__.py
@@ -23,39 +23,42 @@ class Group(BaseGroup):
     pass
 
 
-# def set_gdpr_all(group: Group):
-#     # Sets the payoff by group for all the players in the group
-#     for p in group.get_players():
-#         set_gdpr(p)
+def set_gdpr_all(group: Group):
+    # Sets the payoff by group for all the players in the group
+    for p in group.get_players():
+        set_gdpr(p)
 
 
 class Player(BasePlayer):
-    pass
-    # gdpr_mturk = models.BooleanField(
-    #     initial=False,
-    #     label="Amazon Mechanical Turk Worker ID"
-    # )
-    # gdpr_ip = models.BooleanField(
-    #     initial=False,
-    #     label="IP Address"
-    # )
+    gdpr_mturk = models.BooleanField(
+        initial=False,
+        label="Amazon Mechanical Turk Worker ID"
+    )
+    gdpr_ip = models.BooleanField(
+        initial=False,
+        label="IP Address"
+    )
 
 
-# def set_gdpr(player: Player):
-#     if player.gdpr_mturk is True and player.gdpr_ip is True:
-#         player.participant.vars['gdpr_consent'] = 1
-#         player.participant.vars['is_dropout'] = False
-#     else:
-#         player.participant.vars['gdpr_consent'] = 0
-#         player.participant.vars['is_dropout'] = True
+def set_gdpr(player: Player):
+    if player.gdpr_mturk is True and player.gdpr_ip is True:
+        player.participant.vars['gdpr_consent'] = 1
+        player.participant.vars['is_dropout'] = False
+    else:
+        player.participant.vars['gdpr_consent'] = 0
+        player.participant.vars['is_dropout'] = True
 
 # FUNCTIONS
 # PAGES
 class GDPRConsent(Page):
-    def is_displayed(self):
-        return True
+    form_model = 'player'
+    form_fields = ['gdpr_mturk', 'gdpr_ip']
+    @staticmethod
+    def vars_for_template(player: Player):
+        participant = player.participant
 
 class GDPRWaitPage(WaitPage):
     wait_for_all_groups = True
+    after_all_players_arrive = 'set_gdpr_all'
 
 page_sequence = [GDPRConsent, GDPRWaitPage]
