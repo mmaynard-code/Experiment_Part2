630cafe0ece17bf6232925cb9fb8abaa425d5566
mmaynard-code <80096908+mmaynard-code@users.noreply.github.com>
2021-03-30 12:47:08 +0200

    added loaded var
    loaded var is automatically updated on moving from gdprconsent to gdprwaitpage. Used to track number of loaded participants

--- gdpr_consent/__init__.py
+++ gdpr_consent/__init__.py
@@ -30,7 +30,9 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    loaded = models.IntegerField(
+        initial=0
+    )
     # gdpr_mturk = models.BooleanField(
     #     initial=False,
     #     label="Amazon Mechanical Turk Worker ID"
@@ -52,17 +54,31 @@ class Player(BasePlayer):
 # FUNCTIONS
 # PAGES
 class GDPRConsent(Page):
+    form_model = 'player'
+    form_fields = ['loaded']
     def is_displayed(self):
         return True
 
+
 class GDPRWaitPage(WaitPage):
-    html_text = "<h4>Please wait for all participants to join the experiment</h3>" \
-                "<p>Please be patient, this can take up to 15 minutes.</p>" \
-                "<p>This waiting time has been factored into the time taken to complete the HIT.</p>" \
-                "<p>If at any point the pages are slow to respond, or you receive an error message, please try refreshing the page.</p>" \
-                "<p>If the pages still do not respond, or you continue to receive an error message please contact one of the researchers named in the HIT description, your progress will be saved even if an error occurs.</p>"
-    title_text = "Waiting for other players..."
-    body_text = html_text
+    template_name = 'gdpr_consent/GDPRWaitPage.html'
+    def vars_for_template(self):
+        loaded_list = []
+        for other_player in self.get_others_in_subsession():
+            loaded_list.append(other_player.loaded)
+        loaded = sum(loaded_list) + 1
+        html_text = "<h4>Please wait for all participants to join the experiment</h4>" \
+                    "<p>Please be patient, this can take up to <b>15 minutes</b>.</p>" \
+                    "<p>This waiting time has been factored into the time taken to complete the HIT.</p>" \
+                    "<p>If at any point the pages are slow to respond, or you receive an error message, please try refreshing the page.</p>" \
+                    "<p>If the pages still do not respond, or you continue to receive an error message please contact one of the researchers named in the HIT description, your progress will be saved even if an error occurs.</p>"
+        title_text = "Waiting for other players..."
+        return dict(
+            a0=loaded,
+            loaded=loaded,
+            body_text=html_text,
+            title_text=title_text
+        )
     wait_for_all_groups = True
 
 page_sequence = [GDPRConsent, GDPRWaitPage]
